43f275e52cc3ec25a69393bfd3b9e862c1f83361
benbrowning <benbrowning918@gmail.com>
2023-05-17 06:08:06 +0100

    removed timing decorator

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,5 @@
 import cProfile, pstats, io
 from functools import wraps
-from time import time
 
 def profile(f):
     @wraps(f)
@@ -18,15 +17,3 @@ def profile(f):
         return result
     return wrap
 
-def timing(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        start = time()
-        result = f(*args, **kwargs)
-        end = time()
-
-        print(F"{f.__name__}@{len(args[0])}")
-        print(F"time: {end-start:2.4f} s - profiler can't see inside :(")
-        
-        return result
-    return wrap
